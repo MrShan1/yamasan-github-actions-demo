f068a7f101ca137f450290c20471d233450e4a7a
MrShan1 <568906155@qq.com>
2026-05-21 16:58:35 +0800

    集成Element+图标和Iconfy图标

--- auto-imports.d.ts
+++ auto-imports.d.ts
@@ -6,5 +6,5 @@
 // biome-ignore lint: disable
 export {}
 declare global {
-
+  const Icon: typeof import('@iconify/vue').Icon
 }
--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,46 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted, onBeforeMount } from 'vue'
+import { Icon, loadIcons } from '@iconify/vue'
+import json from '@iconify/json/json/mdi.json'
+
+const iconPrefix = json.prefix // 获取图标前缀
+const icons = Object.keys(json.icons) // 获取所有图标名称
+const iconRef = ref<string>('mdi:home') // 初始化图标
+// console.log(icons)
+
+onBeforeMount(() => {
+  loadIcons(icons.map((icon) => iconPrefix + ':' + icon)) // 预加载所有图标，避免图标闪烁问题
+})
+
+onMounted(() => {
+  // 随机切换图标
+  setInterval(() => {
+    const randomIcon = icons[Math.floor(Math.random() * icons.length)]
+    iconRef.value = iconPrefix + ':' + randomIcon
+  }, 1000)
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container px-4">
+    <h3>Element+ 组件</h3>
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <h3>Iconify 图标</h3>
+    <Icon icon="mdi:home" class="text-red-500" />
+    <!-- 使用动态组件渲染图标 -->
+    <component :is="Icon" :icon="iconRef" />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h3 {
+  margin-top: 15px;
+  margin-bottom: 5px;
 }
 </style>
